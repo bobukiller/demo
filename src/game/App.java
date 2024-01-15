503b15e12b8e12b9909825b2b87dce7d807e797d
bobukiller <1173958100@qq.com>
2024-01-15 11:29:51 +0800

    测试demo

---
+++ src/game/App.java
@@ -0,0 +1,12 @@
+package game;
+
+public class App {
+    public static void main(String[] args) {
+//        LoginJFrame loginJFrame = new LoginJFrame();
+//
+//        RegisterJFrame registerJFrame = new RegisterJFrame();
+
+
+        GameJFrame gameJFrame = new GameJFrame();
+    }
+}
